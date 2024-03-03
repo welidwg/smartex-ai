3af9b17d0eab1a76093ebeb59d1645f682501af2
welidwg <walidgueddari1899@gmail.com>
2024-03-03 13:01:36 +0100

    commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy
 from flask import Flask, request, jsonify
 import cv2
 from flask_cors import CORS
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -18,7 +19,18 @@ def hello_world():  # put application's code here
 def process_image():
     try:
         image = cv2.imdecode(numpy.frombuffer(request.files['image'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
-        text_result=reader.readtext(image)
+        height, width, _ = image.shape
+        red_rectangle_top = max(0, height // 2 - 80)  # Décalage vers le haut de 50 pixels
+        red_rectangle_bottom = min(height, height // 2 + 80)  # Décalage vers le bas de 50 pixels
+        red_rectangle_left = 0
+        red_rectangle_right = width
+
+        cropped_image = image[red_rectangle_top:red_rectangle_bottom, red_rectangle_left:red_rectangle_right]
+        # plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+        # plt.title('Image reçue dans Flask')
+        # plt.axis('off')
+        # plt.show()
+        text_result=reader.readtext(cropped_image)
         text=[result[1] for result in text_result]
         return jsonify({'text': text})
     except Exception as e:
